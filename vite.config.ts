6efc8e5f25ad448afd4ccb13e2c03b65e6efc089
ampp33 <ampp33@gmail.com>
2025-02-26 09:16:12 -0600

    fixed base path and removed dist

--- vite.config.ts
+++ vite.config.ts
@@ -6,6 +6,7 @@ import vueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: "/touch-time-tracker/",
   plugins: [
     vue(),
     vueDevTools(),
